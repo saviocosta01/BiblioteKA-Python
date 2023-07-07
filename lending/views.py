32d953b39e635bbb820ee4fb54ac2934e4b8f9f9
saviocosta01 <savio.costa.silva@gmail.com>
2023-07-07 16:59:00 -0300

    feat: deleting loan history

--- lending/views.py
+++ lending/views.py
@@ -3,11 +3,12 @@ from .models import Lending
 from .serializers import LendingSerializer, DevolutionSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .permissions import LoanHistory
-from django.shortcuts import get_object_or_404
-from users.models import UserModel
-from users.serializers import UserSerializer
+
+# from rest_framework.permissions import IsAuthenticatedOrReadOnly
+# from .permissions import LoanHistory
+# from django.shortcuts import get_object_or_404
+# from users.models import UserModel
+# from users.serializers import UserSerializer
 
 # Create your views here.
 
@@ -21,21 +22,6 @@ class LendingView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         return serializer.save(user=self.request.user)
-
-
-class ViewLoanHistory(generics.ListAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAdminUser]
-
-    serializer_class = LendingSerializer
-    queryset = Lending.objects.all()
-
-    # def get_queryset(self):
-    #     user_id = self.kwargs["pk"]
-    #     user = get_object_or_404(UserModel, pk=user_id)
-    #     Lending_date = Lending.objects.filter(user=user)
-    #     queryset = Lending_date.filter(avaliable=True)
-    #     return queryset
 
 
 class DevolutionView(generics.UpdateAPIView):
