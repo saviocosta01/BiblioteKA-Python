d5b8ebcb4ef476f7ce53df8bf882d4566f387ab9
saviocosta01 <savio.costa.silva@gmail.com>
2023-07-10 17:10:52 -0300

    feat: guessing return logic

--- lending/urls.py
+++ lending/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import LendingView, DevolutionView, TesteLendings
+from .views import LendingView, DevolutionView
 
 urlpatterns = [
     path("lendings/copies/<int:pk>/", LendingView.as_view()),
-    path("lendings/books/<int:pk>/", DevolutionView.as_view()),
-    path("lendings/<int:pk>/", TesteLendings.as_view()),
+    path("lendings/<int:pk>/", DevolutionView.as_view()),
 ]
--- lending/views.py
+++ lending/views.py
@@ -1,9 +1,13 @@
 from rest_framework import generics
 from .models import Lending
-from .serializers import LendingSerializer, DevolutionSerializer, TesteSerializer
+from .serializers import LendingSerializer, DevolutionSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from copies.models import Copies
+from django.shortcuts import get_object_or_404
+from datetime import date, timedelta
+from django.core.exceptions import PermissionDenied
+from django.http import JsonResponse
 
 
 class LendingView(generics.ListCreateAPIView):
@@ -16,6 +20,8 @@ class LendingView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         id = self.kwargs["pk"]
         copies = Copies.objects.get(id=id)
+        if copies.amount <= 0:
+            raise Exception("there are no copies")
         copies.amount -= 1
         copies.save()
         return serializer.save(user=self.request.user, copies=copies)
@@ -24,14 +30,36 @@ class LendingView(generics.ListCreateAPIView):
 class DevolutionView(generics.UpdateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+    lookup_url_kwarg = "pk"
 
     serializer_class = DevolutionSerializer
     queryset = Lending.objects.all()
 
+    def perform_update(self, serializer):
+        lendings_data = Lending.objects.filter(user=self.request.user)
 
-class TesteLendings(generics.ListAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-    lookup_url_kwarg = "pk"
-    queryset = Lending.objects.all()
-    serializer_class = TesteSerializer
+        lending_id = self.kwargs["pk"]
+        # copy = get_object_or_404(Copies, id=copies_id)
+        # lending_instance = Lending.objects.filter(copies=copy)
+
+        lending_instance = Lending.objects.get(id=lending_id, user=self.request.user)
+
+        for i in lendings_data:
+            if not date.today() < i.expiration_date:
+                i.lending_acess = True
+                i.save()
+
+        block_date = date.today() + timedelta(days=lending_instance.lock_time)
+        if date.today() < block_date:
+            raise PermissionDenied()
+
+        lending_instance.avaliable = False
+        lending_instance.save()
+
+        # for j in lending_instance:
+        #     block_date = date.today() + timedelta(days=j.lock_time)
+        #     if date.today() < block_date:
+        #         raise PermissionDenied()
+
+        #     j.avaliable = False
+        #     j.save()
