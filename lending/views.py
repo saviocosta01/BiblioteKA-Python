bc1d93d5dfaf81df9a4dea2e2d5439e127402c5a
Brun0Nun3s <bruunofernandonunes@gmail.com>
2023-07-11 16:14:38 -0300

    feat: documentations lendings and copies

--- lending/views.py
+++ lending/views.py
@@ -6,9 +6,11 @@ from rest_framework.permissions import IsAuthenticated
 from copies.models import Copies
 from datetime import date, timedelta
 from django.core.exceptions import PermissionDenied
+from drf_spectacular.utils import extend_schema
 
 
 class LendingView(generics.ListCreateAPIView):
+   
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     lookup_url_kwarg = "pk"
@@ -23,7 +25,21 @@ class LendingView(generics.ListCreateAPIView):
         copies.amount -= 1
         copies.save()
         return serializer.save(user=self.request.user, copies=copies)
+    @extend_schema(
+        operation_id="lending_get",
+        description="Rota para listar todos os empréstimos de livros",
+        summary="Listar empréstimos"
 
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    @extend_schema(
+        operation_id="lending_post",
+        description="Rota para criar empréstimos de livros",
+        summary="Criar empréstimos"
+    )
+    def post(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 class DevolutionView(generics.UpdateAPIView):
     authentication_classes = [JWTAuthentication]
@@ -52,3 +68,16 @@ class DevolutionView(generics.UpdateAPIView):
 
         lending_instance.avaliable = False
         lending_instance.save()
+
+    @extend_schema(
+        operation_id="devolution_patch",
+        description="Rota para devolver livros",
+        summary="Devolução de livros"
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
+    @extend_schema(
+        exclude=True
+    )
+    def put(self, request, *args, **kwargs):
+        return super().patch(request, *args, **kwargs)
