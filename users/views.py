bd2cb579b876be69729831dc5ab6a354393181b2
Brun0Nun3s <bruunofernandonunes@gmail.com>
2023-07-10 22:56:44 -0300

    feat: users documentation

--- users/views.py
+++ users/views.py
@@ -7,11 +7,28 @@ from rest_framework.views import APIView, Request, Response
 from django.core.mail import send_mail
 from django.conf import settings
 from .permissions import IsAccountOwner,UserAccountOwner
+from drf_spectacular.utils import extend_schema
 
 
 class UserView(generics.ListCreateAPIView):
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
+    @extend_schema(
+        operation_id="users_get",
+        description="Rota para listar todos os usuários",
+        summary="Listar usuários"
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    @extend_schema(
+        operation_id="users_post",
+        description="Rota para cadastrar um estudante ou colaborador da biblioteca",
+        summary="Cadastro de usuários"
+    )
+    def post(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    
+
 
 
 class UserDetailsView(generics.RetrieveUpdateDestroyAPIView):
@@ -21,11 +38,48 @@ class UserDetailsView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
     lookup_url_kwarg = "pk"
 
+    @extend_schema(
+        operation_id="user_get",
+        description="Rota para listar apenas um usuário",
+        summary="Listar usuários, sendo que se for estudante, só podera listar sua própria conta, e colaborador podera listar qualquer uma"
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    @extend_schema(
+        operation_id="user_patch",
+        description="Rota para atualizar usuário",
+        summary="Rota para atualizar dados do usuário, não sendo possivel atualizar a caregoria entre colaborador e estudante",
+        request=UserSerializer,
+        responses={200:UserSerializer}
+       )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+    @extend_schema(
+        operation_id="user_delete",
+        description="Rota para deletar usuário",
+        summary="Rota para excluir um usuário, se o usuário não for admin só poderá excluir sua própria conta"
+    )
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
+    @extend_schema(
+        exclude=True
+    )
+    def put(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
 class UsersLendingsHistory(generics.ListAPIView):
     authentication_classes = [JWTAuthentication]
     queryset= UserModel.objects.all()
     serializer_class = RetrieveLendingUser
     permission_classes = [IsAdminUser]
+
+    
+    @extend_schema(
+        operation_id="user_lendings_get",
+        description="Rota para listar histórico de emprestimos dos usuários",
+        summary="Listar todos os usuários com seus respectivos históricos de emprestimos, somente admin tem acesso a essa rota"
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 
 class UsersLendingsHistoryDetails(generics.RetrieveAPIView):
@@ -34,6 +88,14 @@ class UsersLendingsHistoryDetails(generics.RetrieveAPIView):
     serializer_class = RetrieveLendingUser
     permission_classes = [UserAccountOwner]
     lookup_url_kwarg = "pk"
+
+    @extend_schema(
+        operation_id="user_lending_get",
+        description="Rota para listar histórico de emprestimos do usuário",
+        summary="Listar usuário com seu histórico de emprestimo, somente o dono da conta tem acesso a sua pagina"
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
 
 class SendEmailView(APIView):
     def post(self, req: Request) -> Response:
